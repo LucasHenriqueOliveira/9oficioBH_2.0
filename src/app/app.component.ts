3fa67ca36388b742690c037d774dc26026f831c1
Lucas <lucashen@gmail.com>
2017-09-06 19:11:58 -0300

    change menu layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,40 +6,46 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { IntroPage } from "../pages/intro/intro";
+import { PrivacidadePage } from "../pages/privacidade/privacidade";
+
+import { Constants } from "../app/constants";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+	@ViewChild(Nav) nav: Nav;
 
-  rootPage: any = IntroPage;
+	rootPage: any = IntroPage;
+	nome: string = ''
+	cidade: string = ''
+	constants: any = Constants
 
-  pages: Array<{title: string, component: any}>;
+	pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
+	constructor(public platform: Platform, 
+				public statusBar: StatusBar, 
+				public splashScreen: SplashScreen) {
+		this.initializeApp();
+		this.nome = this.constants.nome
+		this.cidade = this.constants.cidade
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
+	}
 
-  }
+	initializeApp() {
+		this.platform.ready().then(() => {
+		// Okay, so the platform is ready and our plugins are available.
+		// Here you can do any higher level native things you might need.
+		this.statusBar.styleDefault();
+		this.splashScreen.hide();
+		});
+	}
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
-
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+  	openPage(page) {
+		switch (page) {
+			case 'privacidade':
+				this.nav.push(PrivacidadePage);
+				break;
+		}
+	}
 }
